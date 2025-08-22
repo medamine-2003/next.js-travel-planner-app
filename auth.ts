3d179e5b622505f332eec7dad16324b96a9eadc6
medamine-2003 <medamine.jilani@etudiant-enit.utm.tn>
2025-08-22 20:21:30 +0100

    Add files via upload

---
+++ auth.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET, // (optional if using AUTH_SECRET)
+});
